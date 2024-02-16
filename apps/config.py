e2e513645ca36cfe743b0c3f2e6023d482818844
peter <petroritse@gmail.com>
2024-02-16 16:28:33 +0100

    added env

--- apps/config.py
+++ apps/config.py
@@ -7,5 +7,5 @@ class Config:
     load_dotenv()
     SECRET_KEY =  os.environ.get("SECRET_KEY")
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, "new_data.db")
-    SQLALCHEMY_DATABASE_URI =  os.getenv("DATABASE_URL","sqlite:///data.db")
+    SQLALCHEMY_DATABASE_URI =  os.getenv("DATABASE_URL","sqlite:///new_data.db")
     SQLALCHEMY_TRACK_MODIFICATION = False
